7c2ea7902a29a76357d098e1ed6918718a609488
tanyagarg24 <tanyagarg998@gmail.com>
2020-08-02 16:50:30 +0530

    seperate routes and controllers

--- controllers/index.js
+++ controllers/index.js
@@ -1,3 +1,4 @@
+const User = require("./../database/models/User");
 function login(req, res) {
 	if (req.session.user) {
 		res.redirect("/profile");
@@ -6,9 +7,36 @@ function login(req, res) {
 	}
 }
 
+
 function logout(req, res) {
 	req.session.user = null;
 	res.redirect("/login");
 }
+function checkUserAuthentication(req, res){
+	let username = req.body.username;
+	let password = req.body.password;
+	User.findOne({
+		username: username,
+	})
+		.then((user) => {
+			if (user.password == password) {
+				req.session.user = user;
+				res.redirect("/profile");
+			} else {
+				res.send("wrong pass");
+			}
+		})
+		.catch((err) => {
+			res.send("somthing went worng");
+		});
+}
+function viewProfifle (req, res){
+	//console.log(req.session);
+	if (req.session.user) {
+		res.render("profile");
+	} else {
+		res.redirect("/login");
+	}
+}
 
-module.exports = { login, logout };
+module.exports = { login, logout, checkUserAuthentication, viewProfifle};
--- routes/index.js
+++ routes/index.js
@@ -1,35 +1,11 @@
 const router = require("express").Router();
-const { login, logout } = require("./../controllers");
+const { login, logout, checkUserAuthentication ,viewProfifle} = require("./../controllers");
 
 router.get("/login", login);
 router.get("/logout", logout);
+router.post("/login", checkUserAuthentication);
+router.get("/profile", viewProfifle);
 
-router.get("/profile", (req, res) => {
-	console.log(req.session);
-	if (req.session.user) {
-		res.render("profile");
-	} else {
-		res.redirect("/login");
-	}
-});
 
-router.post("/login", (req, res) => {
-	let username = req.body.username;
-	let password = req.body.password;
-	User.findOne({
-		username: username,
-	})
-		.then((user) => {
-			if (user.password == password) {
-				req.session.user = user;
-				res.redirect("/profile");
-			} else {
-				res.send("wrong pass");
-			}
-		})
-		.catch((err) => {
-			res.send("somthing went worng");
-		});
-});
 
 module.exports = router;
