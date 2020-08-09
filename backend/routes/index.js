1755525ca358fce6b3a51926954443b663309e24
aditi1524 <aditi85201@gmail.com>
2020-08-09 06:17:42 -0400

    Removed unwanted lines

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,23 +1,26 @@
 const router = require("express").Router();
-const { login, logout, checkUserAuthentication ,viewProfile, addPost, createPost, feed, registerUser, signup} = require("./../controllers");
+const {
+  login,
+  logout,
+  checkUserAuthentication,
+  viewProfile,
+  addPost,
+  createPost,
+  feed,
+  registerUser,
+  signup,
+} = require("./../controllers");
 const { removeAttribute } = require("../database/models/User");
-const user = require('./user');
-
+const user = require("./user");
 
 router.use("/user", user);
-router.use("/like", )
+router.use("/like");
 
 router.get("/profile", viewProfile);
 router.get("/addPost", addPost);
-router.post("/addPost", createPost)
-router.get("/feed", feed)
-router.get("/signup", signup)
-router.post("/signup", registerUser)
-
+router.post("/addPost", createPost);
+router.get("/feed", feed);
+router.get("/signup", signup);
+router.post("/signup", registerUser);
 
 module.exports = router;
-
-
-get 
-get with ID
-post
