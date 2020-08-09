5207a7539e2475d5cc25cb7fcafa39cd6ddb2aee
tanyagarg24 <tanyagarg998@gmail.com>
2020-08-09 23:40:23 +0530

    update /post route

--- backend/routes/post.js
+++ backend/routes/post.js
@@ -1,12 +1,12 @@
 const router = require("express").Router();
-const {add, getAll, get, deletePost, update} = require("../controllers/post");
+const {addPost, getAllPosts, getPost, deletePost, updatePost} = require("../controllers/post");
 const { isAuthenticated } = require("../controllers");
 
 
-router.post("/", add);
-router.get("/", getAll);
-router.get("/:id", get);
+router.post("/", addPost);
+router.get("/", getAllPosts);
+router.get("/:id", getPost);
 router.delete("/:id", deletePost);
-router.put("/:id", update);
+router.put("/:id", updatePost);
 
 module.exports = router;
