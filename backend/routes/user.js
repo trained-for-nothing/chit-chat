af5ba9c639f01f0b39e49602fecfe152d5aa8b72
Vishal <dhimanv542@gmail.com>
2020-08-10 01:04:13 +0530

    add meaningfull names to controllers

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,11 +1,11 @@
 const router = require("express").Router();
-const {createUser, getAllUsers, getUserById, deleteUser, updateUserById} = require("../controllers/user")
+const {createUser, getAllUsers, getUser, deleteUser, updateUser} = require("../controllers/user")
 
 
 router.post("/", createUser);
 router.get("/", getAllUsers);
-router.get("/:id", getUserById);
-router.put("/:id", updateUserById);
+router.get("/:id", getUser);
+router.put("/:id", updateUser);
 router.delete("/:id", deleteUser);
 
 module.exports = router;
