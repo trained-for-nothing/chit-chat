2dd3b6abf6fbafec79c672b7ac043934e2bbda37
tanyagarg24 <tanyagarg998@gmail.com>
2020-08-09 23:12:44 +0530

    modify /index routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,23 +1,20 @@
 const router = require("express").Router();
-const { login, logout, checkUserAuthentication ,viewProfile, addPost, createPost, feed, registerUser, signup} = require("./../controllers");
-const { removeAttribute } = require("../database/models/User");
+const { login, logout, isAuthenticated} = require("./../controllers");
+const modelUser = require("../database/models/User");
+const modelPost = require("../database/models/Post");
 const user = require('./user');
-
+const post = require('./post');
 
 router.use("/user", user);
-router.use("/like", )
 
-router.get("/profile", viewProfile);
-router.get("/addPost", addPost);
-router.post("/addPost", createPost)
-router.get("/feed", feed)
-router.get("/signup", signup)
-router.post("/signup", registerUser)
+router.use(isAuthenticated);
+router.use("/post", post);
+// router.get("/profile", viewProfile);
+// router.get("/addPost", addPost);
+// router.post("/addPost", createPost)
+// router.get("/feed", feed)
+// router.get("/signup", signup)
+// router.post("/signup", registerUser)
 
 
 module.exports = router;
-
-
-get 
-get with ID
-post
